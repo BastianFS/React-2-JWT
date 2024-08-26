ddda08fda671fc01b054a317e25b7b262d0eb7cd
Bastian Flores <bastianflores@Bastians-MacBook-Pro.local>
2024-08-25 21:25:26 -0400

    fix commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,8 @@ import './App.css'
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './components/Home';
-<<<<<<< HEAD
 //import Pizza from './components/Pizza';
 //import Cart from './components/Cart';
-=======
-import Cart from './components/Cart';
->>>>>>> main
 //import Register from './components/Register';
 //import Login from './components/Login';
 
@@ -17,16 +13,10 @@ function App() {
     <>
       <Navbar/>
       <Home/>
-<<<<<<< HEAD
       {/*<Cart/>*/}
       {/*<Register/>*/}
       {/*<Login/>*/}
       {/*<Pizza/>*/}
-=======
-      <Cart/>
-      {/*<Register/>*/}
-      {/*<Login/>*/}
->>>>>>> main
       <Footer/>
     </>
   );
